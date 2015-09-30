3c99e238dd8594f8ef197eaad890839eb4ddf16e
Bhaskar Rajoriya <bhaskar.rajoriya@gyrix.co>
2015-09-30 16:35:32 +0530

    made angularjs changes to the profile edit page

--- resources/views/home/profileEdit.blade.php
+++ resources/views/home/profileEdit.blade.php
@@ -23,6 +23,22 @@
 
 <link href='http://fonts.googleapis.com/css?family=Roboto:300,400' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<style type="text/css">
+	input[type="text"], select, input[type="submit"], textarea
+	{	
+		border: 1px solid #fff;
+	    height: 38px;
+	    padding: 0 15px;
+        background-color: #fff;
+	    margin: 0px;
+	}
+
+	.addBorder
+	{
+		border-color: #268AD5;
+
+	}
+</style>
 </head>
 
 <body id="profile-edit" ng-controller="profileUpdateController">
@@ -88,7 +104,7 @@
     <input style="display:none;" id="profilePic" type="file" accept="image/*" image="profilePic"/>
     <img ng-show="profilePic" height="180px" width="220px" ng-src="<%profilePic.url%>" class="upload-logo"></div>
     <div class="col-lg-9">
-      <h2 class="company-title"><%edit.name.value%></h2>
+      <h2 class="company-title" ng-bind='businessContact1'></h2>
       <h4 class="company-type">@if($business->businessId) Business Type: {{$business->type}} @else {{$business->type}} @endif</h4>
       <button type="button" class="btn btn-default green"><img src="images/icons/chat.png" class="icon">Contact us</button><button type="button" class="btn btn-default blue"><img src="images/icons/double-right-arrow.png" class="icon">Follow</button>
     </div>
@@ -102,24 +118,34 @@
 	<div class="row">
 		<!-- Sidebar Column -->
 		<div class="col-md-3 sidebar">
-			<div class="page-meta" >
+			<div class="page-meta" ng-init="disabled = true">
 			@if($business->businessId)
-				<span ng-init="edit.name.value='{{$business->name}}';edit.name.disabled='true';edit.name.classStyle = 'profileEditDisableInput'">Contact:</span> <input style="width: 136px" ng-class="edit.name.classStyle" ng-disabled="edit.name.disabled" type="text" ng-model="edit.name.value"/> <a ng-click="makeEditableFunc(edit.name)" data-toggle="modal" data-target="#myModal" ><img src="/images/icons/edit-2.png" class="edit-profile"></a>
+				<span >Contact:</span> 
+				<input style="width: 136px" ng-disabled="disabled" type="text" ng-model="businessContact1"/> 
+				<a data-toggle="modal" data-target="#myModal" ><img src="/images/icons/edit-2.png" class="edit-profile" style="cursor: pointer;"></a>
 				<br>
-				<span ng-init="edit.location.value='{{$business->location}}';edit.location.disabled='true';edit.location.classStyle = 'profileEditDisableInput'">Location:</span> <input style="width: 136px" ng-class="edit.location.classStyle" ng-disabled="edit.location.disabled" type="text" ng-model="edit.location.value"/><a ng-click="makeEditableFunc(edit.location)" data-toggle="modal" data-target="#myModal" ><img src="/images/icons/edit-2.png" class="edit-profile"></a>
+				<span >Location:</span> 
+				<input style="width: 136px" ng-disabled="disabled" type="text" ng-model="businessLocation1"/>
 				<br>
-				<span ng-init="edit.serviceCoverage.value='{{$business->serviceCoverage}}';edit.serviceCoverage.disabled='true';edit.serviceCoverage.classStyle = 'profileEditDisableInput'">Servicing:</span> <input style="width: 136px" ng-class="edit.serviceCoverage.classStyle" ng-disabled="edit.serviceCoverage.disabled" type="text" ng-model="edit.serviceCoverage.value"/><a ng-click="makeEditableFunc(edit.serviceCoverage)" data-toggle="modal" data-target="#myModal" ><img src="/images/icons/edit-2.png" class="edit-profile"></a>
+				<span >Servicing:</span> 
+				<input style="width: 136px" ng-disabled="disabled" type="text" ng-model="businessServicing1"/>
 				<br>
-				<span ng-init="edit.phone.value='{{$business->phone}}';edit.phone.disabled='true';edit.phone.classStyle = 'profileEditDisableInput'">Phone:</span> <input style="width: 136px" ng-class="edit.phone.classStyle" ng-disabled="edit.phone.disabled" type="text" ng-model="edit.phone.value"/><a ng-click="makeEditableFunc(edit.phone)" data-toggle="modal" data-target="#myModal" ><img src="/images/icons/edit-2.png" class="edit-profile"></a>
+				<span >Phone:</span> 
+				<input style="width: 136px" ng-disabled="disabled" type="text" ng-model="businessPhone1"/>
 				<br>
-				<span ng-init="edit.website.value='{{$business->website}}';edit.website.disabled='true';edit.website.classStyle = 'profileEditDisableInput'">Web:</span><input style="width: 136px" ng-class="edit.website.classStyle" ng-disabled="edit.website.disabled" type="text" ng-model="edit.website.value"/><a ng-click="makeEditableFunc(edit.website)" data-toggle="modal" data-target="#myModal" ><img src="/images/icons/edit-2.png" class="edit-profile"></a>
-				<span></span><img src="images/icons/facebook.png" class="social-icon"><img src="/images/icons/twitter.png" class="social-icon"><img src="images/icons/google-plus.png" class="social-icon"><a href="#">
+				<span >Web:</span>
+				<input style="width: 136px" ng-disabled="disabled" type="text" ng-model="businessLink1"/>
+				<span>
+					<img src="images/icons/facebook.png" class="social-icon">
+					<img src="/images/icons/twitter.png" class="social-icon">
+					<img src="images/icons/google-plus.png" class="social-icon">
+				</span>
 				<div class="clearfix spacer2x"></div>
 				<span class="bookmark"><img src="/images/icons/double-right-arrow-color.png" class="icon">2,122 Followers</span>
 				<div class="reviews">
 					<img src="/images/icons/review.png" class="stars"><span class="rating">4.5</span> 12 Reviews
 				</div>
-				 <!-- <div class="modal fade" id="myModal" role="dialog">
+				 <div class="modal fade" id="myModal" role="dialog">
 	                <div class="modal-dialog">
 	                    
 	                    <div class="modal-content">
@@ -130,56 +156,64 @@
 	                    	<form>
 		                        <div class="form-group">
 		                            <label for="inputEmail">Contact:</label>
-		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="business.contact" placeholder="Contact...">
+		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="businessContact" placeholder="Contact...">
 		                        </div>
 		                        <div class="form-group">
 		                            <label for="inputPassword">Location:</label>
-		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="business.location" placeholder="Location...">
+		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="businessLocation" placeholder="Location...">
 		                        </div>
 		                        <div class="form-group">
 		                            <label for="inputPassword">Servicing:</label>
-		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="business.servicing" placeholder="Servicing...">
+		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="businessServicing" placeholder="Servicing...">
 		                        </div>
 		                        <div class="form-group">
 		                            <label for="inputPassword">Phone:</label>
-		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="business.phone" placeholder="Phone...">
+		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="businessPhone" placeholder="Phone...">
 		                        </div>
 		                        <div class="form-group">
 		                            <label for="inputPassword">Web:</label>
-		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="business.link"  placeholder="Website Link...">
+		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="businessLink"  placeholder="Website Link...">
 		                        </div>
 
 		                       <div class="form-group">
 		                            <label for="inputPassword"><img src="images/icons/facebook.png" class="social-icon"></label>
-		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="business.facebook"   placeholder="Facebook...">
+		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="businessFacebook"   placeholder="Facebook...">
 		                        </div>
 		                        <div class="form-group">
 		                            <label for="inputPassword"><img src="/images/icons/twitter.png" class="social-icon"></label>
-		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="business.twitter"  placeholder="twitter...">
+		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="businessTwitter"  placeholder="twitter...">
 		                        </div>
 		                        <div class="form-group">
 		                            <label for="inputPassword"><img src="images/icons/google-plus.png" class="social-icon"></label>
-		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="business.google" placeholder="google +...">
+		                            <input style="margin: 0 !important" type="text" class="form-control" ng-model="businessGoogle" placeholder="google +...">
 		                        </div>
-	                        	<button type="submit" ng-click='updateProfileData()' class="btn btn-primary">Send</button>
+	                        	<!-- <button type="submit" ng-click='updateProfileData()' class="btn btn-primary">Send</button> -->
 	                    	</form>
                       	</div>
                     </div>
-	            </div>  -->
+	            </div> 
 	            <!-- <button type="button" class="btn btn-default edit-this-page no-margin-top"    ><img src="/images/icons/edit-white.png" class="icon">
                     Compose New Message
                 </button> -->
-			@else
-				<span ng-init="edit.name.value='{{$business->name}}';edit.name.disabled='true';edit.name.classStyle = 'profileEditDisableInput'">Username:</span> <input ng-class="edit.name.classStyle" ng-disabled="edit.name.disabled" type="text" ng-model="edit.name.value"/> <a ng-click="makeEditableFunc(edit.name)"><img src="/images/icons/edit-2.png" class="edit-profile"></a></span>
+				@else
+				<span>Username:</span> 
+				<input  type="text" ng-model=""/>
+				
 				<br>
-				<span ng-init="edit.location.value='{{$business->location}}';edit.location.disabled='true';edit.location.classStyle = 'profileEditDisableInput'">Location:</span> <span><input ng-class="edit.location.classStyle" ng-disabled="edit.location.disabled" type="text" ng-model="edit.location.value"/> <a ng-click="makeEditableFunc(edit.location)"><img src="/images/icons/edit-2.png" class="edit-profile"></a></span>
+				<span>Location:</span> 
+				<span>
+					<input  type="text" ng-model=""/> 
+				</span>
 				<br>
-				<span></span><img src="/images/icons/facebook.png" class="social-icon"><img src="/images/icons/twitter.png" class="social-icon"><img src="/images/icons/google-plus.png" class="social-icon"><a href="#"><img src="images/icons/edit-2.png" class="edit-profile"></a>
+				<span></span>
+				<img src="/images/icons/facebook.png" class="social-icon">
+				<img src="/images/icons/twitter.png" class="social-icon">
+				<img src="/images/icons/google-plus.png" class="social-icon">
 				<div class="clearfix spacer2x"></div>
 				<span class="bookmark"><img src="/images/icons/social-circle-blue.png" class="icon">2,122 Connections</span>
 				<br>
 				<span class="bookmark"><img src="/images/icons/double-right-arrow-color.png" class="icon">2,122 Following</span>
-			@endif
+				@endif
 				<div class="edit-page">
 					<button type="button" ng-click="updateProfileData()" class="btn btn-default edit-this-page green"><img src="/images/icons/save.png" class="icon">Save Changes When Done
 					</button>
@@ -187,10 +221,10 @@
 			</div>
 		</div>
 		<!-- Content Column -->
-		<div class="col-md-9 content">
-			<h2 class="content-heading"><img src="/images/icons/file-2.png" class="icon about-us">About Us <a ng-click="makeEditableFunc(edit.about)"><img src="images/icons/edit-2.png" class="edit-profile"></a></h2>
-			<p ng-init='edit.about.value="{{$business->about}}";edit.about.disabled="true";edit.about.classStyle = "profileEditDisableInput"'>
-				<textarea ng-class="edit.about.classStyle" style="margin: 0px 15px; width: 885px; height: 165px;" ng-disabled="edit.about.disabled" ng-model="edit.about.value"></textarea>
+		<div class="col-md-9 content" ng-init="editable=true" >
+			<h2 class="content-heading"><img src="/images/icons/file-2.png" class="icon about-us">About Us <a ng-click="editable = !editable"><img src="images/icons/edit-2.png" class="edit-profile" style="cursor: pointer;"></a></h2>
+			<p>
+				<textarea style="resize:none;width: 885px; height: 165px;" ng-disabled='editable' ng-model="aboutUs" ng-class="{addBorder: true === !editable}"></textarea>
 			</p>
 			<h2 class="content-heading"><img src="/images/icons/services.png" class="icon services">Services <a href="#"><img src="images/icons/edit-2.png" class="edit-profile"></a></h2>
 			<ul class="fa-ul">
@@ -476,17 +510,37 @@
 	           })
 	            .controller('profileUpdateController', function($scope, $http) {
 
-	            	$scope.makeEditableFunc = function(inputId) {
-	            		inputId.disabled = !(inputId.disabled);
-				        if (inputId.disabled)
-				            inputId.classStyle = "profileEditDisableInput";
-				        else
-				            inputId.classStyle = "profileEditEnableInput";
+	            	$scope.init = function () {
+	            		$scope.businessContact = '{{$business->name}}';
+	            		$scope.businessLocation = '{{$business->location}}';
+		            	$scope.businessServicing = '{{$business->serviceCoverage}}';
+		            	$scope.businessPhone = '{{$business->phone}}';
+		            	$scope.businessLink = '{{$business->website}}';
+		            	$scope.businessGoogle = 'www.google.com';
+		            	$scope.businessTwitter = 'www.twitter.com';
+		            	$scope.businessFacebook = 'www.facebook.com';
+		            	$scope.aboutUs = '{{$business->about}}';
+
+		            	//not for edit only to display
+
+		            	$scope.businessContact1 = '{{$business->name}}';
+	            		$scope.businessLocation1 = '{{$business->location}}';
+		            	$scope.businessServicing1 = '{{$business->serviceCoverage}}';
+		            	$scope.businessPhone1 = '{{$business->phone}}';
+		            	$scope.businessLink1 = '{{$business->website}}';
+
 	            	};
 
-	            	$scope.chooseImage = function(imageId) {
+	            	$scope.init();
+						            	
+
+
+	            	
+
+	            	/*$scope.chooseImage = function(imageId) {
 	            		$('#'+imageId).click();
-					};
+					};*/
+
 	                $scope.single = function() {
 	                	var profilePic = $scope.profilePic;
 	                	var coverPic = $scope.coverPic;
@@ -506,35 +560,28 @@
 							console.log(response);
 						});
 	                };
+
 	                $scope.updateProfileData = function(){
 	                	
 	                	@if($business->businessId)
 
-	                	$scope.edit.name.disabled = true;
-	                	$scope.edit.location.disabled = true;
-	                	$scope.edit.about.disabled = true;
-	                	$scope.edit.serviceCoverage.disabled = true;
-	                	$scope.edit.phone.disabled = true;
-	                	$scope.edit.website.disabled = true;
-
-	                	var data = {
-	                		name : $scope.edit.name.value,
-	                		location : $scope.edit.location.value,
-	                		about : $scope.edit.about.value,
-	                		serviceCoverage : $scope.edit.serviceCoverage.value,
-	                		phone : $scope.edit.phone.value,
-	                		website : $scope.edit.website.value
+	                	var data = 
+	                	{
+	                		name : $scope.businessContact,
+	                		location : $scope.businessLocation,
+	                		about : $scope.aboutUs,
+	                		serviceCoverage : $scope.businessServicing,
+	                		phone : $scope.businessPhone,
+	                		website : $scope.businessLink
 	                	};
 
 	                	@else
 
-	                	$scope.edit.name.disabled = true;
-	                	$scope.edit.location.disabled = true;
-	                	$scope.edit.about.disabled = true;
+	                	
 	                	var data = {
-	                		name : $scope.edit.name.value,
-	                		location : $scope.edit.location.value,
-	                		about : $scope.edit.about.value
+	                		name : $scope.businessContact,
+	                		location : $scope.businessLocation,
+	                		about : $scope.aboutUs
 	                	};
 
 	                	@endif
@@ -545,7 +592,7 @@
 						    params: data
 						 })
 						.success(function(response) {
-							
+							$('#myModal').modal('hide');	
 						});
 				    };
 	            });
